06ca9f4abdd65b533eb01071d0cb769fe3c1f10b
anthonyramos47 <anthonyramos47@gmail.com>
2024-02-05 23:34:33 +0300

    New opt

--- Sphere_approx.bash
+++ Sphere_approx.bash
@@ -1,7 +1,7 @@
 source /home/anthony/anaconda3/etc/profile.d/conda.sh
 
-# conda activate hananJ
-# python hanan/testing_TF.py parameters.json
+conda activate hananJ
+python hanan/testing_TF.py parameters.json
 
 conda activate geo
 python Quad_remesh/stress_remesher.py parameters.json TriMesh

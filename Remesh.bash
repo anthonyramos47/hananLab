920d4b2946ff456918e5475174d6c931b2f57c41
anthonyramos47 <anthonyramos47@gmail.com>
2024-04-07 10:07:08 +0300

    Remeshings Test

--- Remesh.bash
+++ Remesh.bash
@@ -2,7 +2,7 @@ source /home/anthony/anaconda3/etc/profile.d/conda.sh
 conda activate geo
 
 Exp_dir="/home/anthony/Hanan Lab/hananLab/QS_project/experiments/"
-Exp_name="Florian_A_20_TA_60.pickle"
+Exp_name="Florian_A_25_TA_45.pickle"
 
 file_path=$Exp_dir$Exp_name
 echo $Exp_dir+$Exp_name
